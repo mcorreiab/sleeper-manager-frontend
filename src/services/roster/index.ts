8eae93555cf6b352b8ff71710960fa9d5fd9af53
Murilo Correia <murilo.correiab@gmail.com>
2021-04-20 20:28:33 -0300

    refactor: Use api url

--- src/services/roster/index.ts
+++ src/services/roster/index.ts
@@ -3,7 +3,7 @@ import RosterModel from "./model";
 
 const getRostersByUserId = async (userId: string): Promise<RosterModel[]> => {
   const response = await axios.get<RosterModel[]>(
-    `https://sleeper-manager.mcorreia.dev/rosters/user/userId/${userId}/unavailable`
+    `https://sleeper-manager-api.mcorreia.dev/rosters/user/userId/${userId}/unavailable`
   );
   return response.data;
 };
