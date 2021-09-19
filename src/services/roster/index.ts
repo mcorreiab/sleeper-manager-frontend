1e552eae4d211bae434b837da2cdb9e68e9a2be3
Murilo Correia <murilo.correiab@gmail.com>
2021-09-19 13:34:49 -0300

    fix: Treat 404 correctly

--- src/services/roster/index.ts
+++ src/services/roster/index.ts
@@ -2,10 +2,18 @@ import axios from "@/config/axios";
 import { RosterModel } from "./model";
 
 const getRostersByUserId = async (userId: string): Promise<RosterModel[]> => {
-  const response = await axios.get<RosterModel[]>(
-    `https://sleeper-manager-api.mcorreia.dev/rosters/user/userId/${userId}/unavailable`
-  );
-  return response.data;
+  try {
+    const response = await axios.get<RosterModel[]>(
+      `https://sleeper-manager-api.mcorreia.dev/rosters/user/userId/${userId}/unavailable`
+    );
+    return response.data;
+  } catch (error: any) {
+    if (error.response.status === 404) {
+      return Promise.resolve([]);
+    }
+
+    throw error;
+  }
 };
 
 export default getRostersByUserId;
